dc38090bd5ff0125496fa6ea74edceec48d77ccd
Jan Ove Kongshaug <jan.ove.kongshaug@novari.no>
2026-02-16 14:23:44 +0100

    FFS-1583: Bump versions
    - Newest internal libraries
    - Java 25

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,7 @@
-FROM gcr.io/distroless/java21
+FROM gcr.io/distroless/java25:nonroot
 ENV TZ="Europe/Oslo"
 ENV JAVA_TOOL_OPTIONS=-XX:+ExitOnOutOfMemoryError
 WORKDIR /app
 COPY build/libs/*.jar ./app.jar
 EXPOSE 8080
-USER nonroot
 CMD ["app.jar"]
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,21 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.5.9"
+    id("org.springframework.boot") version "3.5.10"
     id("io.spring.dependency-management") version "1.1.7"
-    kotlin("jvm") version "2.1.10"
-    kotlin("plugin.spring") version "2.1.10"
+    kotlin("jvm") version "2.3.10"
+    kotlin("plugin.spring") version "2.3.10"
     id("org.jlleitschuh.gradle.ktlint") version "13.1.0"
+    id("com.github.ben-manes.versions") version "0.53.0"
 }
 
-group = "no.fintlabs"
+group = "no.novari"
 version = "0.0.1-SNAPSHOT"
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(21))
+        languageVersion.set(JavaLanguageVersion.of(25))
     }
 }
 
@@ -34,7 +36,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
 
-    implementation("no.novari:flyt-web-instance-gateway:1.3.6")
+    implementation("no.novari:flyt-web-instance-gateway:2.0.0")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
@@ -53,4 +55,17 @@ tasks.withType<Test> {
 
 tasks.named("check") {
     dependsOn("ktlintCheck")
+}
+
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return !isStable
+}
+
+tasks.named<DependencyUpdatesTask>("dependencyUpdates") {
+    rejectVersionIf {
+        isNonStable(candidate.version)
+    }
 }
